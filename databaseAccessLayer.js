3cfe4d93f9f66c2f4245deb8c429f97c6d0f46c4
Ryan <ryancollicutt22@gmail.com>
2021-04-07 20:46:05 -0700

    damn-right

--- databaseAccessLayer.js
+++ databaseAccessLayer.js
@@ -2,7 +2,7 @@ const database = include('/databaseConnection');
 
 
 function grabRestaurants(callback) {
-	let sqlQuery = "SELECT name, description FROM restaurant";
+	let sqlQuery = "SELECT restaurant_id, name, description FROM restaurant";
 	database.query(sqlQuery, (err, results, fields) => {
 		if (err) {
 			callback(err, null);
